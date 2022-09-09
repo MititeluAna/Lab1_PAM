c15e1f188a931b5e6cc41de39d1f3431426ed9cb
MititeluAna <ana.mititelu2001@gmail.com>
2022-09-09 15:20:48 +0300

    first commit

---
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        b_calculer.setOnClickListener{
+            val masa = et_masa.text.toString().toDouble()
+        val inalt = et_inalt.text.toString().toDouble()
+
+        indice = (masa / (inalt * inalt)).toSting()
+        }
+    }
+}
