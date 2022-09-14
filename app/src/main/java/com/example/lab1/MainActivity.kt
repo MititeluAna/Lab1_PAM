5d9de815cb118eee2f792556645ff2aa0bcfecc5
MititeluAna <ana.mititelu2001@gmail.com>
2022-09-14 12:31:12 +0300

    Laboratorul 1 finished

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,18 +2,26 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        b_calculer.setOnClickListener{
-            val masa = et_masa.text.toString().toDouble()
-        val inalt = et_inalt.text.toString().toDouble()
+        val inalt = findViewById<TextView>(R.id.inalt);
+        val masa = findViewById<TextView>(R.id.masa);
+        val indice = findViewById<TextView>(R.id.indice);
+        val buton = findViewById<Button>(R.id.but);
 
-        indice = (masa / (inalt * inalt)).toSting()
-            //vineive
+        buton.setOnClickListener {
+            val masa = masa.text.toString().toDouble();
+            val inalt = inalt.text.toString().toDouble();
+
+            val calcul = (masa / (inalt * inalt));
+
+            indice.text = calcul.toString();
         }
     }
 }
